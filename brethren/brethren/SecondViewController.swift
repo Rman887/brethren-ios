8cd6fd6232c609ba7e75987b70c51caf910099d3
Arman Siddique <arman.siddique887@gmail.com>
2018-10-20 14:52:57 -0400

    Created xcode project

---
+++ brethren/brethren/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  brethren
+//
+//  Created by Arman Siddique on 10/20/18.
+//  Copyright © 2018 Cornelius and Co. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
